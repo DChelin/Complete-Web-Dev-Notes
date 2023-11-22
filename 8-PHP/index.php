ecebfb21c076e91b4ac4ac78222cd98eaa7bb292
DChelin <devlynchelin6@gmail.com>
2023-11-22 11:58:28 +0200

    PHP Notes
    PHP Notes

---
+++ 8-PHP/index.php
@@ -0,0 +1,10 @@
+<?php
+//echo = prinitng statement
+//CHALLENGE TRY TO BOLD THE TEXT WITH HTML
+//Intentionally breaking code to enable error reporting.
+//Created a user.ini file to error log
+
+    echo "<strong>Hello World!</strong>";
+
+?>
+
