9ded947386907c156af4e9c0921c48ee39b95383
DChelin <55988126+DChelin@users.noreply.github.com>
2023-12-17 18:48:27 +0200

    Update index.php

--- 8-PHP/php-contact-form/index.php
+++ 8-PHP/php-contact-form/index.php
@@ -8,9 +8,7 @@
         <meta name="viewport" content="width=devic-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-wa-compatible" content="ie=edge">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
         
-    
     </head>
     <body>
         <div class="container"><!--- start of container-->
@@ -36,5 +34,14 @@
             </form><!--- end of the form-->
 
         </div><!---end of conatiner div-->
+
+        <!--- download of jQuery-->
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+        <!--- download Bootstrap-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+   
+        <!---- TO DO JS --->
+        
     </body>
+
 </html>
